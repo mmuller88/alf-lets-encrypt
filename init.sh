42cd982757ec861ca70e31e912b05167d0d7a369
Martin Müller <damadden88@googlemail.com>
2020-05-03 17:08:02 +0200

    press in the jwt

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 Content-Type: multipart/mixed; boundary="//"
 MIME-Version: 1.0
 
+#!/usr/bin/env bash
+
+yum update -y
+amazon-linux-extras install docker
+service docker start
+usermod -a -G docker ec2-user
+
 curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
