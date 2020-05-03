9782d82bf7e89aafea9d505dd7d95c89bddd31a5
Martin Müller <damadden88@googlemail.com>
2020-05-03 17:04:27 +0200

    press in the jwt

--- init.sh
+++ init.sh
@@ -1,29 +1,6 @@
 Content-Type: multipart/mixed; boundary="//"
 MIME-Version: 1.0
 
---//
-Content-Type: text/cloud-config; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="cloud-config.txt"
-
-#cloud-config
-cloud_final_modules:
-- [scripts-user, always]
-
---//
-Content-Type: text/x-shellscript; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="userdata.txt"
-
-#!/usr/bin/env bash
-
-yum update -y
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-
 curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
@@ -38,4 +15,3 @@ sed -i '\|logs/postgres|d' ./docker-compose.yml
 
 # put in auto start
 # cp ./scripts/alfresco /etc/init.d
---//
