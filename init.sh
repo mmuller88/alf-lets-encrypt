799a7e6669b2316b5e8cfcf0b36752b2f4b091be
Martin Müller <damadden88@googlemail.com>
2020-05-12 18:57:28 +0200

    install docker

--- init.sh
+++ init.sh
@@ -5,8 +5,12 @@ sudo yum install docker -y
 service docker start
 usermod -a -G docker ec2-user
 
+echo starting docker compose download
 curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+echo finished docker compose download
+
+sleep 5
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source /.nvm/nvm.sh
